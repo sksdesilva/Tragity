e96ddfed167f9549627c68b122bdc2e6a48dc4f4
sksdesilva <kavindusandeepadesilva@gmail.com>
2022-12-06 21:29:00 +0530

    Stationaries.js

---
+++ src/components/Category/Gifts/Gifts.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Gifts = () =>{
+    return(
+        <>
+
+
+
+
+        </>
+    );
+}
+ export default Gifts;
---
+++ src/components/Stationaries/Stationaries.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Stationaries = () =>{
+    return(
+        <>
+
+
+
+
+        </>
+    );
+}
+ export default Stationaries;
